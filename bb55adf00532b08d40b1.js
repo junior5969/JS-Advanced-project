add4022520e1b9e03ee0fbe819fe6caa6f1cd541
Chiara <clodiauni@gmail.com>
2025-07-19 16:22:29 +0200

    Updates

---
+++ bb55adf00532b08d40b1.js
@@ -0,0 +1,1 @@
+import"../css/style.css";import{setupMenuToggle}from"./navbar.js";import{setupSearch}from"./search.js";document.addEventListener("DOMContentLoaded",()=>{setupMenuToggle(),setupSearch()});
