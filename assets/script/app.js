d98e9dddc4bc2049c18cfa5f75aa2fbc74a112f3
Chiara <clodiauni@gmail.com>
2025-07-19 16:27:29 +0200

    Fix loader

--- assets/script/app.js
+++ assets/script/app.js
@@ -3,8 +3,10 @@
 import "../css/style.css";
 import { setupMenuToggle } from "./navbar.js";
 import { setupSearch } from "./search.js";
+import { hiddenLoader } from './loader';
 
 document.addEventListener("DOMContentLoaded", () => {
+   hiddenLoader();
   setupMenuToggle();
   setupSearch();
 });
