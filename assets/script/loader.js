bd810343b5270271fc90a1f5c23cabb312a59167
Chiara <clodiauni@gmail.com>
2025-07-19 16:20:41 +0200

    Rename hiddenLoader function

--- assets/script/loader.js
+++ assets/script/loader.js
@@ -5,7 +5,7 @@ export function showLoader() {
   loader.classList.remove("hidden");
 }
 
-export function hiddennLoader() {
+export function hiddenLoader() {
   const loader = document.querySelector("#loader-overlay");
   loader.classList.add("hidden");
 }
